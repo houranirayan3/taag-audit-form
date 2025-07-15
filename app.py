dd62fd79dfd2c0f827bf8036dc27231372306457
houranirayan3 <rayan.hourani@hotmail.com>
2025-07-15 18:33:47 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="TAAG Audit Form",
+    layout="wide",
+)
+
+st.markdown("""
+    <h1 style='color:#004d99;'>TAAG Website Audit Tool</h1>
+    <p style='font-size:18px;'>Welcome! Use the left menu to navigate through each section of the form.</p>
+""", unsafe_allow_html=True)
