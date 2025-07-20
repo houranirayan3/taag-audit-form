acd046d335f46a93735ffaf5c36b02f9a9f132a8
houranirayan3 <rayan.hourani@hotmail.com>
2025-07-20 12:38:09 +0300

    Update firebase_db.py

--- firebase_db.py
+++ firebase_db.py
@@ -2,11 +2,15 @@ import streamlit as st
 from google.cloud import firestore
 from google.oauth2 import service_account
 
-# Load credentials from secrets
-creds = service_account.Credentials.from_service_account_info(
-    st.secrets["firestore"]
-)
+def get_firestore_client():
+    credentials = service_account.Credentials.from_service_account_info(st.secrets["firestore"])
+    return firestore.Client(credentials=credentials, project=credentials.project_id)
 
-# Initialize Firestore client
-db = firestore.Client(credentials=creds, project=st.secrets["firestore"]["project_id"])
+def list_all_documents(collection_name):
+    db = get_firestore_client()
+    docs = db.collection(collection_name).stream()
+    return [doc.id for doc in docs]
 
+def save_form_data(collection_name, document_id, data):
+    db = get_firestore_client()
+    db.collection(collection_name).document(document_id).set(data)
